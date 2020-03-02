f0d4c4fc4bedfc262917e1bc9780aa65309e8618
root <root@mahendra.lalani>
2020-03-02 23:23:13 +0530

    remove list.py

--- list.py
+++
@@ -1,9 +0,0 @@
-import json
-import boto3
-import botocore
-import os
-
-conn = boto3.client('s3') 
-for key in s3.list_objects(Bucket='smashistaging',Prefix='fbtest2/')['Contents']:
-    print("Hii")
-    print(key['Key'])
